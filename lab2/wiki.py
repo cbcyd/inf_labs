2b1f2e3a57556a815442ffe7a8bee81b08efd5b0
cbcyd <a@MacBook-Air-Vasilij.local>
2023-10-07 16:00:38 +0300

    edit lab2

---
+++ lab2/wiki.py
@@ -0,0 +1,39 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from wikipediaapi import Wikipedia
+
+wiki_wiki = Wikipedia('MyProjectName (merlin@example.com)', 'en')
+
+class Item(BaseModel):
+    title: str
+    summary: str
+    #text: str
+
+class Wiki(BaseModel):
+    title: str
+
+app = FastAPI()
+
+@app.get("/path/{title}")
+async def with_path(title: str):
+    page_py = wiki_wiki.page(title)
+    #wiki = Wiki(title=page_py.title, summary=page_py.summary, text=page_py.text)
+    item = Item(title=page_py.title, summary=page_py.summary)#, text=page_py.text)
+    #return {"Summary:": wiki.summary} if page_py.exists() else 'Page not found'
+    return item
+
+@app.get("/query/")
+async def with_query(title: str = None):
+    page_py = wiki_wiki.page(title)
+    #wiki = Wiki(title=page_py.title, summary=page_py.summary, text=page_py.text)
+    item = Item(title=page_py.title, summary=page_py.summary)#, text=page_py.text)
+    #return {"Title:": wiki.title, "Summary:": wiki.summary, "Text": wiki.text} if page_py.exists() else 'Page not found'
+    return item
+
+
+@app.post("/post/")
+async def with_post(wiki: Wiki):
+    page_py = wiki_wiki.page(wiki.title)
+    wiki = Item(title=page_py.title, summary=page_py.summary)#, text=page_py.text)
+    return wiki
